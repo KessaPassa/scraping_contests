fd510b02a3c5ef775ff8f34d112a009d3232084d
KessaPassa <k.someya.gyao@gmail.com>
2020-01-27 21:54:33 +0900

    second commit

---
+++ ContestInfo.py
@@ -0,0 +1,66 @@
+import requests
+import json
+import datetime as dt
+from dotenv import load_dotenv
+import os
+from os.path import join
+
+load_dotenv(verbose=True)
+dotenv_path = join('./', '.env')
+load_dotenv(dotenv_path)
+
+reserved_list = set()
+post_interval = 60 * 60 * 2
+
+
+class ContestInfo:
+
+    def __init__(self, time, name, url):
+        base_url = 'https://atcoder.jp'
+
+        self.time = dt.datetime.strptime(time, '%Y-%m-%d %H:%M:%S')
+        self.name = name
+        self.url = base_url + url
+        self.posted = False
+
+        is_reserved = False
+        # reserved_listが空の時は無条件に入れる
+        if len(reserved_list) == 0:
+            is_reserved = False
+        else:
+            # classを格納しているので一つずつ取り出して確認する
+            for r in list(reserved_list):
+                if (r.time == self.time) and (r.name == self.name) and (r.url == self.url):
+                    is_reserved = True
+
+        # reverved_listと重複していなければ追加する
+        if not is_reserved:
+            reserved_list.add(self)
+
+    # 指定時間内のコンテストがあるかどうか
+    def judge(self):
+        can_post = False
+
+        diff = (self.time - dt.datetime.today())
+        # diff = (dt.datetime.today() - self.time)
+
+        # 2時間以内のコンテストがあるなら通知を許可する
+        if (not self.posted) and (0 <= diff.days) and (diff.seconds <= post_interval):
+            can_post = True
+        # コンテストの開催時間が過ぎていたら配列から削除する
+        elif diff.days < 0:
+            reserved_list.remove(self)
+
+        print(reserved_list)
+        return can_post
+
+    # slackに通知を出す
+    def post(self):
+        if self.judge():
+            webhook_url = os.environ.get('WEBHOOK_URL')
+            requests.post(
+                webhook_url,
+                json.dumps({'text': '{}\n{}\n{}'.format(self.time, self.name, self.url)}),
+                headers={'Content-Type': 'application/json'}
+            )
+            self.posted = True
---
+++ main.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+import schedule
+import time
+from ContestInfo import ContestInfo
+
+
+# 空白があるので調整して一行の文字列にする
+def ajust_one_message(message):
+    line = message.text.rsplit()
+    return ' '.join(line)
+
+
+# コンテスト情報をスクレイピングする
+def scraping_contest_info():
+    # url = 'https://atcoder.jp/home'
+    # html = requests.get(url).text
+    html = open('atcoder.html')
+
+    soup = BeautifulSoup(html, 'html.parser')
+    div = soup.find('div', attrs={'id': 'contest-table-upcoming'})
+    content = div.find_all('a')
+
+    contest_list = []
+
+    # コンテスト開始時間とコンテスト名がバラバラになっているので2つずつ回して1つにまとめる
+    for i in range(0, len(content), 2):
+        # タイムゾーン(%z)が+でついているので省く
+        time = ajust_one_message(content[i]).split('+')[0]
+        name = ajust_one_message(content[i + 1])
+        # urlはダブルクォーテーションで区切られている
+        url = str(content[i + 1]).replace(' ', '').split('\"')[1]
+
+        contest_list.append(ContestInfo(time, name, url))
+
+    return contest_list
+
+
+# 定期実行
+def job():
+    contest_list = scraping_contest_info()
+    for c in contest_list:
+        c.post()
+
+
+if __name__ == '__main__':
+    # 設定時間ごとにjob関数を実行する
+    schedule.every(1).hours.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
